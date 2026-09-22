db482aa662e81faf56ec22fa0b5518588810c2d8
Manas <rmnsdeo@gmail.com>
2026-09-22 23:48:37 +0530

    Add initial Spring Boot application structure with Maven support and example components

---
+++ MyApp/src/main/java/com/manas/myapp/Dev.java
@@ -0,0 +1,10 @@
+package com.manas.myapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Working on an awesome project");
+    }
+}
---
+++ MyApp/src/main/java/com/manas/myapp/MyAppApplication.java
@@ -0,0 +1,18 @@
+package com.manas.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MyAppApplication.class, args);
+
+        Dev devObj = context.getBean(Dev.class);
+
+        devObj.build();
+    }
+
+}
